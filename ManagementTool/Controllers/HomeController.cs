4d7fa6e253c38aaf5b829507aa315b3e77dbf142
Web Developer <iamarsalan@hotmail.com>
2017-08-17 12:19:21 +0400

    home controller

--- ManagementTool/Controllers/HomeController.cs
+++ ManagementTool/Controllers/HomeController.cs
@@ -9,8 +9,7 @@ namespace ManagementTool.Controllers
     public class HomeController : Controller
     {
         public ActionResult Index()
-        {
-            // make changes
+        {            
             ViewData["SubTitle"] = "Welcome in ASP.NET MVC 5 INSPINIA SeedProject ";
             ViewData["Message"] = "It is an application skeleton for a typical MVC 5 project. You can use it to quickly bootstrap your webapp projects.";
 
