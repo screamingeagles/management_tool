861d70745e357dbe56cd78efa56035a01205b747
Web Developer <iamarsalan@hotmail.com>
2017-08-17 12:17:53 +0400

    first commit

---
+++ ManagementTool/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ManagementTool.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            // make changes
+            ViewData["SubTitle"] = "Welcome in ASP.NET MVC 5 INSPINIA SeedProject ";
+            ViewData["Message"] = "It is an application skeleton for a typical MVC 5 project. You can use it to quickly bootstrap your webapp projects.";
+
+            return View();
+        }
+
+        public ActionResult Welcome()
+        {
+            return View();
+        }
+    }
+}
