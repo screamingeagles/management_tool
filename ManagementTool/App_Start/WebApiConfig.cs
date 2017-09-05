1cd087c6a070cf055c4b343fbd0711d146abd2a1
Web Developer <iamarsalan@hotmail.com>
2017-09-05 15:28:28 +0400

    json filters added

--- ManagementTool/App_Start/WebApiConfig.cs
+++ ManagementTool/App_Start/WebApiConfig.cs
@@ -1,7 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Net.Http.Headers;
+using System.Collections.Generic;
 
 namespace ManagementTool
 {
@@ -13,9 +14,11 @@ namespace ManagementTool
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
-                defaults: new { id = RouteParameter.Optional }
+                routeTemplate: "web/api/{controller}/{action}/{id}",
+                defaults: new { action = "Index", id = RouteParameter.Optional }
             );
+
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
         }
     }
 }
---
+++ ManagementTool/Controllers/web/TaskController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ManagementTool.Controllers.web
+{
+    public class TaskController : ApiController
+    {
+        // GET: localhost:29771/web/api/Task/Get
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Task/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Task
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Task/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Task/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
